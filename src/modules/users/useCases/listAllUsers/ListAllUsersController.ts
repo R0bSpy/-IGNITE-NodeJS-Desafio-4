61f6a5497e2a25364b04d83b193aac87cb8f2958
Lipe <robson.santos3@estudante.ifms.edu.br>
2021-08-26 18:46:59 -0300

    Finalizando desafio 4

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,8 +6,7 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { id } = request.headers;
-    const user_id = <string>id;
+    const { user_id } = request.headers;
 
     try {
       const users = this.listAllUsersUseCase.execute({
