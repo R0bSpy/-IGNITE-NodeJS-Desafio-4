b04ea42ffe1372849293144ae76ac54002c66366
Lipe <robson.santos3@estudante.ifms.edu.br>
2021-08-26 18:40:48 -0300

    Finalizando desafio 4

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,17 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { id } = request.headers;
+    const user_id = <string>id;
+
+    try {
+      const users = this.listAllUsersUseCase.execute({
+        user_id,
+      });
+      return response.json(users);
+    } catch {
+      return response.status(400).json({ error: "Mensagem do erro" });
+    }
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,19 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("Mensagem do erro");
+    }
+
+    if (!user.admin) {
+      throw new Error("Usuário não é administrador");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
